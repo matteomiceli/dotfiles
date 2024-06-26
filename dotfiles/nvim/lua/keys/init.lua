930136c9e486cd81985c2b64f9b2af772a377ff4
Matteo Miceli <71541064+matteomiceli@users.noreply.github.com>
2024-06-25 22:51:37 -0700

    [nvim] update keys

--- dotfiles/nvim/lua/keys/init.lua
+++ dotfiles/nvim/lua/keys/init.lua
@@ -1,2 +1,7 @@
--- Open file explorer in split right view
-vim.keymap.set("n", "<leader>oo", ':Vex! <Enter>')
+-- File explorer bindings
+vim.keymap.set("n", "<leader>e", ':Ex <Enter>')
+vim.keymap.set("n", "<leader>o", ':Vex! <Enter>')
+
+-- Window and tab management
+vim.keymap.set("n", "<leader>nt", ':tabe <Enter>') -- new tab
+vim.keymap.set("n", "<leader>q", ':q<Enter>') -- close
