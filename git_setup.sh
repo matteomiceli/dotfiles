aece2afd3f7cc6fa6bfd592fcc89855d1f5ff84d
Matteo Miceli <71541064+matteomiceli@users.noreply.github.com>
2024-08-01 20:08:41 -0700

    [git_setup] set default editor

--- git_setup.sh
+++ git_setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 git config --global user.email "omatteomiceli@gmail.com"
+echo "email set"
 git config --global user.name "matteomiceli"
+echo "username set"
+git config --global core.editor "nvim"
+echo "editor set"
