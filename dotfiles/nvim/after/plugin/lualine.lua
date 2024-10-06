7da4083704887a4a5656d2111ccd3640a607380b
matteomiceli <omatteomiceli@gmail.com>
2024-10-06 11:47:42 -0700

    [nvim] update lualine config

--- dotfiles/nvim/after/plugin/lualine.lua
+++ dotfiles/nvim/after/plugin/lualine.lua
@@ -2,8 +2,8 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
+    component_separators = { left = '│', right = '│' },
+    section_separators = { left = '', right = '' },
     disabled_filetypes = {
       statusline = {},
       winbar = {},
@@ -20,8 +20,8 @@ require('lualine').setup {
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { 'filename' },
-    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_c = {{ 'filename', path=1 }},
+    lualine_x = { 'encoding', 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' }
   },
