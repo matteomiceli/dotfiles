f0af7b6bf4f0dfdcb7bc981cf407fbb129de9852
Matteo Miceli <71541064+matteomiceli@users.noreply.github.com>
2025-07-23 13:43:03 -0700

    [nivm] terminal mode escape remap

--- dotfiles/nvim/lua/keys/init.lua
+++ dotfiles/nvim/lua/keys/init.lua
@@ -5,3 +5,6 @@ vim.keymap.set("n", "<leader>o", ':Vex! <Enter>')
 -- Window and tab management
 vim.keymap.set("n", "<leader>nt", ':tabe <Enter>') -- new tab
 vim.keymap.set("n", "<leader>q", ':q<Enter>') -- close
+
+-- Terminal mode
+vim.keymap.set("t", "<ESC>",'<C-\\><C-n>')
