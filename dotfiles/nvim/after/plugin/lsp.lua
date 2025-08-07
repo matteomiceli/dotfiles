1c7620bbed8c78b95e3819b2474224f9beb9acd6
Matteo Miceli <71541064+matteomiceli@users.noreply.github.com>
2025-08-07 11:00:57 -0700

    fix: completion windows have borders

--- dotfiles/nvim/after/plugin/lsp.lua
+++ dotfiles/nvim/after/plugin/lsp.lua
@@ -39,6 +39,10 @@ cmp.setup({
     ['<Tab>'] = cmp_action.luasnip_supertab(),
     ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
   }),
+  window = {
+    completion = cmp.config.window.bordered({ border = 'rounded' }),
+    documentation = cmp.config.window.bordered({ border = 'rounded' }),
+  }
 })
 
 lsp_zero.setup()
