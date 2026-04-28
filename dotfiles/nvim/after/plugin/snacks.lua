a1aae66a0c26060d9424f4141b8bb748daf1217e
Matteo Miceli <71541064+matteomiceli@users.noreply.github.com>
2026-04-28 15:42:53 -0700

    [nvim] use tab in snacks to goto next and prev files
    Added custom key mappings for picker input in snacks.nvim configuration.

--- dotfiles/nvim/after/plugin/snacks.lua
+++ dotfiles/nvim/after/plugin/snacks.lua
@@ -1,7 +1,17 @@
 -- Config for snacks.nvim plugins
 
 require("snacks").setup({
-  picker = { enabled = true },
-  explorer = { enabled = true },
+  picker = {
+    enabled = true,
+    win = {
+      input = {
+        keys = {
+          ["<S-Tab>"] = { "list_up", mode = { "i", "n" } },
+          ["<Tab>"] = { "list_down", mode = { "i", "n" } },
+        }
+      }
+    }
+  },
+  explorer = { enabled = true }
   -- find setup in keys
 })
